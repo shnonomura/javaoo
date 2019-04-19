e892dba7b10f11df571f2386c0a26bd20cea8e04
SERGIO <45925582+shnonomura@users.noreply.github.com>
2019-04-19 18:48:35 -0300

    Create 01-printf.java

---
+++ docs/01-printf.java
@@ -0,0 +1,11 @@
+// Este é o primeiro arquivo no GitHub
+// Farei uma aplicação Java utilizando a método System.out.printf
+public class mostraMensagem()
+{
+    
+    public static void main(strinb[],args)
+    {
+        mensagem = "GitHub";
+        system.out.printf("Bem-vindo ao meu %s!", mensagem)
+    }
+}
