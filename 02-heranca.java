efade6e3f44e70a2ac2a3a8a7db58723597f8b99
SERGIO <45925582+shnonomura@users.noreply.github.com>
2019-04-19 17:29:28 -0300

    Create 02-heranca.java

---
+++ 02-heranca.java
@@ -0,0 +1,2 @@
+/** O objetivo deste exemplo é introduzir o conceito de herança, ou seja, a capacidade de criar classes adquirindo os membros de uma classe existente ( sem copiar e colar o código). Lembrando que os membros de uma classe são seus membros e atributos.
+*/
