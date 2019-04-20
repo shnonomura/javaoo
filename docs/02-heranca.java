52371cffac2328801de39a5201e2cbe01d257503
SERGIO <45925582+shnonomura@users.noreply.github.com>
2019-04-19 21:32:44 -0300

    Update 02-heranca.java

--- docs/02-heranca.java
+++ docs/02-heranca.java
@@ -1,6 +1,5 @@
-/** O objetivo deste exemplo é introduzir o conceito de herança, ou seja, a capacidade de criar classes adquirindo os membros de uma classe existente ( sem copiar e colar o código). Lembrando que os membros de uma classe são seus membros e atributos. 
-**/
-
+'''/** O objetivo deste exemplo é introduzir o conceito de herança, ou seja, a capacidade de criar classes adquirindo os membros de uma classe existente ( sem copiar e colar o código). Lembrando que os membros de uma classe são seus membros e atributos. 
+**/'''
 ## Project Overview
 In this project, you will apply basic machine learning concepts on data collected for housing prices in the Boston, Massachusetts area to predict the selling price of a new home. You will first explore the data to obtain important features and descriptive statistics about the dataset. Next, you will properly split the data into testing and training subsets, and determine a suitable performance metric for this problem. You will then analyze performance graphs for a learning algorithm with varying parameters and training set sizes. This will enable you to pick the optimal model that best generalizes for unseen data. Finally, you will test this optimal model on a new sample and compare the predicted selling price to your statistics.
 
